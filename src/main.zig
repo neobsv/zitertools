d6fdd00163b4e3d10a20ca48390c7afb820a449a
Bhargav Srinivasan <bsvasan92@icloud.com>
2020-10-17 22:41:12 -0700

    implemented map, reduce and filter

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,34 @@ const tallocator = std.testing.allocator;
 const warn = std.debug.warn;
 const assertEqual = std.testing.expectEqual;
 
+pub fn reduce(
+    comptime func: anytype, 
+    iterable: []@typeInfo(@TypeOf(func)).Fn.args[0].arg_type.?,
+    comptime init: @typeInfo(@TypeOf(func)).Fn.return_type.? 
+) @typeInfo(@TypeOf(func)).Fn.return_type.? {
+    var ans = init;
+    for (iterable) | item | {
+        ans = func(ans, item);
+    }
+    return ans;
+}
+
+fn add8(a: u8, b: u8) u8 { 
+    return a + b; 
+}
+fn mul8(a: u8, b: u8) u8 { 
+    return a * b; 
+}
+
+test "Reduce" {
+    var A = [_]u8{1, 2, 4};
+    assertEqual(reduce(add8, &A, 0), 7);
+    assertEqual(reduce(mul8, &A, 0), 0);
+    assertEqual(reduce(mul8, &A, 1), 8);
+    warn("\r\n", .{});
+}
+
+
 pub fn Iterator(comptime T: type) type {
     return struct {
         allocator: *std.mem.Allocator,
@@ -42,10 +70,10 @@ pub fn Iterator(comptime T: type) type {
 fn printTest(comptime T: type, iter: *Iterator(T), ans: []T) void{
     var i: usize = 0;
     while (iter.next()) | item | {
+        // warn("\r\n ans: {} item: {} ", .{ans[i], item.*});
         assertEqual(ans[i], item.*);
         i += 1;
     }
-    warn("\r\n", .{});
 }
 
 test "Iterator" {
@@ -73,7 +101,84 @@ test "Iterator" {
     defer iter2.deinit();
 
     printTest(u8, &iter2, B.items);
+    warn("\r\n", .{});
 
+}
+
+pub fn map(
+    allocat: *std.mem.Allocator,
+    comptime func: anytype, 
+    iterable: []@typeInfo(@TypeOf(func)).Fn.args[0].arg_type.?
+) Iterator(@typeInfo(@TypeOf(func)).Fn.args[0].arg_type.?) {
+    const RType = @typeInfo(@TypeOf(func)).Fn.args[0].arg_type.?;
+    var ans: []RType  = allocat.alloc(RType, iterable.len) catch unreachable;
+    defer allocat.destroy(ans.ptr);
+    for (ans) | _, i | { ans[i] = 0;}
+    
+    var i: usize = 0;
+    while (i < iterable.len) : ( i += 1 ) {
+        ans[i] =  func(iterable[i]);
+    }
+
+    return Iterator(RType).init(allocat, ans);
+}
+
+fn addOne(a: u8) u8 {
+    return a + @intCast(u8, 1);
+}
+
+test "Map" {
+    var A = [_]u8{'a', 'b', 'c'};
+    var ans = [_]u8{'b', 'c', 'd'};
+
+    var res = map(tallocator, addOne, &A);
+    defer res.deinit();
+
+    printTest(u8, &res, &ans);
+    warn("\r\n", .{});
+}
+
+pub fn filter(
+    allocat: *std.mem.Allocator,
+    comptime func: anytype, 
+    iterable: []@typeInfo(@TypeOf(func)).Fn.args[0].arg_type.?
+) Iterator(@typeInfo(@TypeOf(func)).Fn.args[0].arg_type.?) {
+    const RType = @typeInfo(@TypeOf(func)).Fn.args[0].arg_type.?;
+    var ans: []RType  = allocat.alloc(RType, iterable.len) catch unreachable;
+    defer allocat.destroy(ans.ptr);
+    for (ans) | _, i | { ans[i] = 0;}
+    
+    var i: usize = 0;
+    var j: usize = 0;
+    while (i < iterable.len) : ( i += 1 ) {
+        if (func(iterable[i]) == true) {
+            ans[j] = iterable[i];
+            j += 1;
+        }
+    }
+
+    _ = allocat.shrink(ans, j);
+    ans.len = j;
+
+    return Iterator(RType).init(allocat, ans);
+}
+
+fn isLessThan10(a: u8) bool {
+    if ( a < 10 ){
+        return true;
+    }
+    return false;
+}
+
+test "Filter" {
+    var A = [_]u8{1, 'a', 2, 'b', 3, 'c', 'd', 'e'};
+    var ans = [_]u8{1, 2, 3, 0, 0, 0, 0, 0};
+
+    var res = filter(tallocator, isLessThan10, &A);
+    defer res.deinit();
+
+    printTest(u8, &res, &ans);
+    warn("\r\n", .{});
 }
 
 pub fn accumulate(
@@ -123,7 +228,7 @@ test "Accumulate" {
     printTest(u32, &res, &ans1);
     printTest(u32, &res2, &ans2);
     printTest(u32, &res3, &ans3);
-
+    warn("\r\n", .{});
 }
 
 
