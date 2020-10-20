29fe25e5e1dffc96a7264573b503b89d1edae385
Bhargav Srinivasan <bsvasan92@icloud.com>
2020-10-19 17:34:11 -0700

    compile time map, reduce, filter

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ This is an attempt to port the itertools library from python to zig, in order to
 paradigm to the language. Maintains high speed by eliminating temporary allocations, and moving through 
 slices using an iterator. The library also includes some constructs such as map, filter and reduce which 
 are part of the python builtin library which are essential for functional programming.
+
+And of course, their compile time counterparts!
+
 Suggestions and contributions are welcome.
 
 ## Generic Iterator
---
+++ src/ctmain.zig
@@ -0,0 +1,143 @@
+const std = @import("std");
+const builtin = std.builtin;
+const warn = std.debug.warn;
+const assertEqual = std.testing.expectEqual;
+
+pub fn ctreduce(
+    comptime func: anytype, 
+    comptime iterable: []@typeInfo(@TypeOf(func)).Fn.args[0].arg_type.?,
+    comptime init: @typeInfo(@TypeOf(func)).Fn.return_type.? 
+) @typeInfo(@TypeOf(func)).Fn.return_type.? {
+    comptime var ans = init;
+    inline for (iterable) | item | {
+        ans = func(ans, item);
+    }
+    return ans;
+}
+
+fn add8(a: u8, b: u8) u8 { 
+    return a + b; 
+}
+fn mul8(a: u8, b: u8) u8 { 
+    return a * b; 
+}
+
+test "Reduce" {
+    comptime {
+        comptime var A = [_]u8{1, 2, 4};
+        assertEqual(ctreduce(add8, &A, 0), 7);
+        assertEqual(ctreduce(mul8, &A, 0), 0);
+        assertEqual(ctreduce(mul8, &A, 1), 8);
+    }
+    warn("\r\n", .{});
+}
+
+
+pub fn CTIterator(comptime T: type) type {
+    return struct {
+        iterable: []const T,
+        index: usize = 0,
+        len: usize,
+
+        const Self = @This();
+
+        fn incr(it: *Self) void {
+            it.index += 1;
+        }
+
+        pub fn next(it: *Self) ?*const T {
+            if(it.index < it.len) {
+                defer it.incr();
+                return &it.iterable[it.index];
+            }
+            return null;
+        }
+
+        pub fn init(iter: []const T) Self {
+            return Self{
+                .iterable = iter,
+                .len = iter.len
+            };
+        }
+    };
+}
+
+fn printTest(comptime T: type, comptime iter: *CTIterator(T), comptime ans: []T) void{
+    comptime var i: usize = 0;
+    inline while (iter.next()) | item | {
+        // warn("\r\n ans: {} item: {} ", .{ans[i], item.*});
+        assertEqual(ans[i], item.*);
+        i += 1;
+    }
+}
+
+test "Iterator" {
+    comptime {
+        comptime var A = [_]u8{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        comptime var iter = CTIterator(u8).init(&A);
+        printTest(u8, &iter, &A);
+    }
+    warn("\r\n", .{});
+
+}
+
+pub fn ctmap(
+    comptime func: anytype, 
+    comptime iterable: []@typeInfo(@TypeOf(func)).Fn.args[0].arg_type.?
+) CTIterator(@typeInfo(@TypeOf(func)).Fn.args[0].arg_type.?) {
+    comptime var rtype = @typeInfo(@TypeOf(func)).Fn.args[0].arg_type.?;
+    comptime var ans: []const rtype  = &[0]rtype{};
+    
+    inline for(iterable) | item, i | {
+        ans = ans ++ &[1]rtype{ func(item) };
+    }
+
+    return CTIterator(rtype).init(ans);
+}
+
+fn addOne(comptime a: u8) u8 {
+    return a + @intCast(u8, 1);
+}
+
+test "Map" {
+    comptime {
+        comptime var A = [_]u8{'a', 'b', 'c'};
+        comptime var ans = [_]u8{'b', 'c', 'd'};
+        comptime var res = ctmap(addOne, &A);
+        printTest(u8, &res, &ans);
+    }
+    warn("\r\n", .{});
+}
+
+pub fn ctfilter(
+    comptime func: anytype, 
+    comptime iterable: []@typeInfo(@TypeOf(func)).Fn.args[0].arg_type.?
+) CTIterator(@typeInfo(@TypeOf(func)).Fn.args[0].arg_type.?) {
+    comptime var rtype = @typeInfo(@TypeOf(func)).Fn.args[0].arg_type.?;
+    comptime var ans: []const rtype  = &[0]rtype{};
+    
+    inline for(iterable) | item, i | {
+        if (func(item) == true) {
+            ans = ans ++ &[1]rtype{ item };
+        }
+    }
+
+    return CTIterator(rtype).init(ans);
+}
+
+fn isLessThan10(a: u8) bool {
+    if ( a < 10 ){
+        return true;
+    }
+    return false;
+}
+
+test "Filter" {
+    comptime {
+        comptime var A = [_]u8{1, 'a', 2, 'b', 3, 'c', 'd', 'e'};
+        comptime var ans = [_]u8{1, 2, 3};
+        comptime var res = ctfilter(isLessThan10, &A);
+        printTest(u8, &res, &ans);
+    }
+    warn("\r\n", .{});
+}
